7c04deea08018dc87e04cca0a788f4dffd4623ba
tdoodchenko <tldoodchenko@gmail.com>
2020-05-06 15:45:25 -0400

    commented out javascript. JS file was just to force animation for effect

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,10 @@
 // Force a hover to see the effect
-const share = document.querySelector('.share');
+// const share = document.querySelector('.share');
 
-setTimeout(() => {
-  share.classList.add("hover");
-}, 1000);
+// setTimeout(() => {
+//   share.classList.add("hover");
+// }, 1000);
 
-setTimeout(() => {
-  share.classList.remove("hover");
-}, 3000);
+// setTimeout(() => {
+//   share.classList.remove("hover");
+// }, 3000);
